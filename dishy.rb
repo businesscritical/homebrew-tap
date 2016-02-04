7a0c898a09b4765ca3440e5c6ada5f04181754ad
Dan Wendorf <dwendorf@pivotal.io>
2016-02-04 10:21:53 -0800

    Add jq as dependency for dishy

--- dishy.rb
+++ dishy.rb
@@ -4,6 +4,7 @@ class Dishy < Formula
   homepage 'https://github.com/businesscritical/dishy'
   url 'https://github.com/businesscritical/dishy/archive/0.1.0.tar.gz'
   sha256 '5cf016fe49f669569b85066349b82ecf10be4d8477e04b3a25e3f00ed873b549'
+  depends_on 'jq'
 
   def install
     bin.install 'dishy', 'imgcat'
