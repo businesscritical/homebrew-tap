dfe68311a77c637125fbe7c666fec8c33a177923
Dan Wendorf <dwendorf@pivotal.io>
2016-02-12 16:11:01 -0800

    Bump dishy to 0.1.1

--- dishy.rb
+++ dishy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dishy < Formula
   homepage 'https://github.com/businesscritical/dishy'
-  url 'https://github.com/businesscritical/dishy/archive/0.1.0.tar.gz'
-  sha256 '5cf016fe49f669569b85066349b82ecf10be4d8477e04b3a25e3f00ed873b549'
+  url 'https://github.com/businesscritical/dishy/archive/0.1.1.tar.gz'
+  sha256 '8814dfc306ddb8bd2fd199765f2eb3aeeb20e5cd282de817f1fcb24ef68df010'
   depends_on 'jq'
 
   def install
