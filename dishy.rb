bd6606a3de46d53cc5a9595626c8f0b13f3db526
Dan Wendorf <dwendorf@pivotal.io>
2016-02-17 17:08:18 -0800

    Bump dishy to 0.1.6
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- dishy.rb
+++ dishy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dishy < Formula
   homepage 'https://github.com/businesscritical/dishy'
-  url 'https://github.com/businesscritical/dishy/archive/0.1.5.tar.gz'
-  sha256 '6a0e5b37bf6b6351d7492471c9a322494f177cae1dafea717f85ddcaafa6a627'
+  url 'https://github.com/businesscritical/dishy/archive/0.1.6.tar.gz'
+  sha256 '174bc5f5cbb941f1a76e593338e40bf9cb2c93aa14b19d325cc72e723f4f9ab1'
   depends_on 'jq'
 
   def install
