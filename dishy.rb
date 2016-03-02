01cbc94d247cc43c18abefa9b6ee47189ad1f46b
Dan Wendorf <dwendorf@pivotal.io>
2016-03-02 12:50:07 -0800

    Dishy formula (mostly) passes brew audit
    `brew audit --online --strict dishy`
    
    - Add description
    - Add basic test block
    - Prefer double-quoted strings
    - `require 'formula'` is unnecessary
    - Add newlines where appropriate
    
    Dishy is still too young (< 30 days old) and not notable enough (<10
    forks, <10 watchers and <20 stars) for inclusion in the main homebrew
    repo, but when those are resolved, this formula should be in good shape.

--- dishy.rb
+++ dishy.rb
@@ -1,12 +1,16 @@
-require 'formula'
-
 class Dishy < Formula
-  homepage 'https://github.com/businesscritical/dishy'
-  url 'https://github.com/businesscritical/dishy/archive/0.1.6.tar.gz'
-  sha256 '174bc5f5cbb941f1a76e593338e40bf9cb2c93aa14b19d325cc72e723f4f9ab1'
-  depends_on 'jq'
+  desc "Displays fun GIFs on iTerm2"
+  homepage "https://github.com/businesscritical/dishy"
+  url "https://github.com/businesscritical/dishy/archive/0.1.6.tar.gz"
+  sha256 "174bc5f5cbb941f1a76e593338e40bf9cb2c93aa14b19d325cc72e723f4f9ab1"
+
+  depends_on "jq"
 
   def install
-    bin.install 'dishy'
+    bin.install "dishy"
+  end
+
+  test do
+    system bin/"dishy"
   end
 end
